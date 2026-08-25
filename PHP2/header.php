88a4abeee468315400e7760e9d864bd92ea17245
remelie-byte <gecko_carvajal@smccnasipit.edu.ph>
2026-08-25 17:40:48 +0800

    Create header.php

---
+++ PHP2/header.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Output #2</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+
+<nav>
+    <a href="index.php">Home</a>
+    <a href="register.php">Register</a>
+    <a href="login.php">Login</a>
+    <a href="forgot_password.php">Forgot Password</a>
+</nav>
+
+<div class="container">
