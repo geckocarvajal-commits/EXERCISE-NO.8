d8e37a7c05ccc53a5a2247c20a79807d27e2c9ca
remelie-byte <gecko_carvajal@smccnasipit.edu.ph>
2026-08-25 17:41:00 +0800

    Create login.php

---
+++ PHP2/login.php
@@ -0,0 +1,17 @@
+<?php include "header.php"; ?>
+
+<h1>Login</h1>
+
+<form method="POST">
+
+    <label>Email</label>
+    <input type="email" name="email" required>
+
+    <label>Password</label>
+    <input type="password" name="password" required>
+
+    <button type="submit">Login</button>
+
+</form>
+
+<?php include "footer.php"; ?>
