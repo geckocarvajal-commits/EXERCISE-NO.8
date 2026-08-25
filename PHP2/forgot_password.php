70e1ba9136c6250f1cf70edf97c4efe4b5afbeaa
remelie-byte <gecko_carvajal@smccnasipit.edu.ph>
2026-08-25 17:40:42 +0800

    Create forgot_password.php

---
+++ PHP2/forgot_password.php
@@ -0,0 +1,16 @@
+<?php include "header.php"; ?>
+
+<h1>Forgot Password</h1>
+
+<p>Enter your email address to request a password reset.</p>
+
+<form method="POST">
+
+    <label>Email</label>
+    <input type="email" name="email" required>
+
+    <button type="submit">Reset Password</button>
+
+</form>
+
+<?php include "footer.php"; ?>
