142389ceb90c4ff066c7d6a3acc6230917c35286
remelie-byte <gecko_carvajal@smccnasipit.edu.ph>
2026-08-25 17:40:54 +0800

    Create index.php

---
+++ PHP2/index.php
@@ -0,0 +1,13 @@
+<?php include "header.php"; ?>
+
+<h1>Welcome</h1>
+
+<p>
+    Welcome to the PHP Registration System.
+</p>
+
+<p>
+    Please register an account or login to continue.
+</p>
+
+<?php include "footer.php"; ?>
